c696ed31ea44e3ec2799aecf013a350572a074d4
ThiagoAmaralDev <thiagoamaraldev@gmail.com>
2026-04-09 22:06:53 -0300

    criando o evento da tabuada (na inserção de números e caso não tenha envio de números)

---
+++ 04- tabuada/guiado/js/script.js
@@ -0,0 +1,21 @@
+// Seleção de elementos
+
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+
+
+// Funções
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.prevenDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    console.log(multiplicationNumber, multiplicatorNumber)
+})
